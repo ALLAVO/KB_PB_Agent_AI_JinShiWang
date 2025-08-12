eba9666706eb18c831c159286ff74f97d78a56d8
allavo <hjchokr2018@gmail.com>
2025-08-12 16:45:14 +0900

    Update secrets configuration

--- app/test.py
+++ app/test.py
@@ -5,3 +5,4 @@ width, height = img.size
 # 하단 20픽셀 제거 예시
 cropped = img.crop((0, 0, width, height - 300))
 cropped.save('/Users/hyungjuncho/Documents/SNU_BFA/KB_capstone/KB_PB_Agent_AI_JinShiWang/frontend/src/assets/client_profile/천소연.png')
+# change anything
